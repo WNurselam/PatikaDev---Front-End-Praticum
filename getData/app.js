7cd55076d57ee6ac1a034bea8a07f64a45278081
Nurselam SAYAN <77880259+WNurselam@users.noreply.github.com>
2023-03-27 15:09:49 +0300

    Update app.js

--- getData/app.js
+++ getData/app.js
@@ -1,3 +1,3 @@
-import fetchData from "./main.js";
+import fetchData from "./main.js"; // fetchData fonksiyonunu dahil etme
 
 fetchData(1) // main.js içerisinde bulunan fonksiyonu id parametresi girme ve çağırma
